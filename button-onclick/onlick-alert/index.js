3d8d7a61a5d3cb9775c26238aeb4eeb1294d69bd
Reshav Thakur <reshav.thakur@aimcomely.com>
2022-07-11 23:02:29 +0530

    learnt onclick

---
+++ button-onclick/onlick-alert/index.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("btn")
+button.addEventListener("mouseout", function () {
+    alert("Button is clicked")
+})
+const showAlert = () => {
+    alert("Button is clicked")
+}
---
+++ button-onclick/show-info/index.js
@@ -0,0 +1,7 @@
+
+const showValue = () => {
+    var nameValue = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    alert(`Your name is ${nameValue} Your email is ${email} and your message is ${message}`)
+}

a29daf8c652410fa35be568c8a672aa13456041a
Reshav Thakur <reshav.thakur@aimcomely.com>
2022-08-26 20:12:11 +0530

    placing center

---
+++ Async Await/index.js
@@ -0,0 +1,8 @@
+// let api = "https://newsapi.org/v2/everything?q=apple&from=2022-07-25&to=2022-07-25&sortBy=popularity&apiKey=d6c47e6d8f2848368145e8f8c0a4e227"
+// let fetchData = async () => {
+//     let response = await fetch(api);
+//     let data = await response.json()
+//     console.log(data)
+// }
+// fetchData();
+document.getElementById("para").innerHTML = "this is my edited para graph"

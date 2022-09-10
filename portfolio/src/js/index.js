26779f708316c7d2e07bac748f7e8101f3f53012
Reshav Thakur <reshav.thakur@aimcomely.com>
2022-09-10 22:44:47 +0530

    contact section

---
+++ portfolio/src/js/index.js
@@ -0,0 +1,43 @@
+
+$('.about').click(function () {
+    $('html,body').animate({
+        scrollTop: $('.aboutme').offset().top
+    },
+        'slow');
+});
+$('.skill').click(function () {
+    $('html,body').animate({
+        scrollTop: $('.skills_section').offset().top
+    },
+        'slow');
+});
+$('.education_Btn').click(function () {
+    $('html,body').animate({
+        scrollTop: $('.education').offset().top
+    },
+        'slow');
+});
+$('.project_Btn').click(function () {
+    $('html,body').animate({
+        scrollTop: $('.project').offset().top
+    },
+        'slow');
+});
+$('.vmp').click(function () {
+    $('html,body').animate({
+        scrollTop: $('.project').offset().top
+    },
+        'slow');
+});
+$('.contact_Btn').click(function () {
+    $('html,body').animate({
+        scrollTop: $('.contact').offset().top
+    },
+        'slow');
+});
+$('.primary').click(function () {
+    $('html,body').animate({
+        scrollTop: $('.contact').offset().top
+    },
+        'slow');
+});

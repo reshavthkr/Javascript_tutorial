5531e0bde1d1c2ac100c3aca221f8297aa7afb37
reshavthkr <reshavthkr@gmail.com>
2022-12-02 22:47:43 +0530

    lecture 1

--- var_vs_let/index.js
+++ var_vs_let/index.js
@@ -1,11 +1,15 @@
-function abc() {
-    var a = 10
-    {
-        let p = 20
-        console.log(p)
-    }
-    console.log(p)
-    console.log(a)
+function calculateSum() {
+    let a = document.getElementById("fistnumber").value
+    let b = document.getElementById("secondnumber").value
+    let sum = a + b
+    let message = `1st number is ${a}, 2nd number is ${b}  and their sum is equal to ${sum} `
+    let text = document.getElementById("text")
+    text.innerText = message
 }
 
-abc()
+// function prinNameWithMessage(str) {
+//     console.log(`Hi, ${str} How are you!`)
+// }
+// prinNameWithMessage("Reshav Thakur")
+
+
